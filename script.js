68e4559a8fcd495a7f90e35f10b84c2447c0e9c9
ChristianGuillen173 <guillenchristian173@gmail.com>
2025-01-18 12:45:06 -0300

    Front-End-Pronto

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function toggleOtherServiceInput() {
     otherServiceContainer.style.display = "none";
   }
 }
+
+document.getElementById("resume").addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const fileSizeMB = file.size / (1024 * 1024);
+    if (fileSizeMB > 5) {
+      alert("O arquivo deve ter no máximo 5MB.");
+      this.value = ""; // Limpa o campo
+    }
+  }
+});
